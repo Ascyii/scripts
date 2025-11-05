2685fa6afe5932f7bbaa978d014138a59d22e693
Jonas Hahn <jonashahn1@gmx.net>
2025-11-05 23:05:02 +0100

    auto up  23:05:00  up   4:27,  2 users,  load average: 0.60, 0.54, 0.49

--- shell/toggle_grayscale.sh
+++ shell/toggle_grayscale.sh
@@ -3,6 +3,8 @@
 HOUR=$(date +%H)
 if [ "$HOUR" -ge 19 ] || [ "$HOUR" -lt 7 ]; then
   hyprshade on grayscale-custom
+  dunstctl set-paused true
 else
   hyprshade off
+  dunstctl set-paused false
 fi
